eadfce2952e7b14e2e4a84fb3e84b646a7ba54c0
José Hélio Veríssimoo Jr <jose.helio@gmail.com>
2024-02-09 22:47:59 -0300

    feat: Marc view

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-import { copyContent, findBooksByIsbn, updateForm } from "./utils/handlers.js";
-import { resetRender } from "./render/interfaces.js";
+import {
+  copyContent,
+  findBooksByIsbn,
+  setButtonsClickEvent,
+  updateForm,
+} from "./utils/handlers.js";
+import {
+  renderMenu,
+  renderResponse,
+  resetRender,
+} from "./render/interfaces.js";
 
 window.addEventListener("load", async () => {
   const formSearch = document.getElementById("search-form");
@@ -8,9 +17,26 @@ window.addEventListener("load", async () => {
   const buttonViewMarc = document.querySelector(".submenu #marc");
   const buttonViewForm = document.querySelector(".submenu #form");
   const marcCode = document.getElementById("marc-code");
+  const buttonMarcCode = marcCode.querySelector(".button-copy");
 
   const inputSearch = document.getElementById("isbn");
-  inputSearch.value = "123 9788565358668 9788598486130 978-8544001516";
+  inputSearch.value = "9788565358668 123 9788598486130 978-8544001516";
+  if (localStorage.length > 0) {
+    resetRender();
+    // localStorage.removeItem("isMarkView");
+    // Object.keys(localStorage).forEach((isbn) => {
+    //   if (isbn !== "currentId") {
+    //     renderMenu(isbn);
+    //   }
+
+    //   setButtonsClickEvent(JSON.parse(localStorage.getItem(isbn)));
+    // });
+    // formSearch.style.display = "none";
+    // nav.style.display = "flex";
+    // formResponse.parentElement.parentElement.style.display = "block";
+
+    // renderResponse(JSON.parse(Object.values(localStorage)[0]));
+  }
 
   formSearch.addEventListener("submit", findBooksByIsbn);
 
@@ -23,10 +49,16 @@ window.addEventListener("load", async () => {
   buttonViewMarc.addEventListener("click", () => {
     formResponse.style.display = "none";
     marcCode.style.display = "flex";
+    localStorage.setItem("isMarkView", true);
+    renderResponse();
   });
 
   buttonViewForm.addEventListener("click", () => {
-    formResponse.style.display = "flex";
+    formResponse.style.display = "block";
     marcCode.style.display = "none";
+    localStorage.removeItem("isMarkView");
+    renderResponse();
   });
+
+  buttonMarcCode.addEventListener("click", copyContent);
 });
